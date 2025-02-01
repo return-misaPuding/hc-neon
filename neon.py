4ebfeb9195d757df2996680c9fdc45f1ce1fd24b
return-misaPuding <70533713+return-misaPuding@users.noreply.github.com>
2025-02-01 14:41:07 +0100

    Fixed in neon emulator
    do not loop linearthings()!!!

--- neon.py
+++ neon.py
@@ -1,4 +1,3 @@
-#64x32 display with a rainbow animation, probably a clock function too
 import adafruit_display_text.label
 import board
 import displayio
@@ -9,12 +8,12 @@ import colorsys
 import time
 import datetime
 displayio.release_displays()
-m = rgbmatrix.RGBMatrix(width=64, height=32, bit_depth=4,rgb_pins=[board.D6, board.D5, board.D9, board.D11, board.D10, board.D12],
+m = rgbmatrix.RGBMatrix(width=64, height=32, bit_depth=5,rgb_pins=[board.D6, board.D5, board.D9, board.D11, board.D10, board.D12],
     addr_pins=[board.A5, board.A4, board.A3, board.A2],
     clock_pin=board.D13, latch_pin=board.D0, output_enable_pin=board.D1)
 d = framebufferio.FramebufferDisplay(m, auto_refresh=False)
 clr = 0.0
-hexclr= 0xffffff
+decclr = 0xffff00
 con1 = ""
 con2 = ""
 def freshdate(dateobj):
@@ -22,24 +21,93 @@ def freshdate(dateobj):
     con1 = dateobj.strftime("%X")
     con2 = f"{dateobj.strftime('%d')}/{dateobj.strftime('%m')} {dateobj.strftime('%a')}"
 def coloring():
-    global hexclr
-    clr += 0.1 #rainbow
+    global decclr, clr
+    clr += 0.01 #rainbow
+    print(clr)
     r,g,b = colorsys.hsv_to_rgb(clr,1,1)
-    hexclr = int(hex(r*256^3+g*256^2+b*256))
+    print(f"{r};{g};{b}")
+    r *= 0xff0000
+    g *= 0xff00
+    b *= 0xff
+    print(f"{r}:{g}:{b}")
+    r = round(r)
+    g = round(g)
+    b = round(b)
+    print(f"rounded {r} {g} {b}")
+    decclr = r+g+b
+    print(decclr)
+    print(hex(decclr))
+dayclr = 0xffffff
+def decideday(inputdate):
+    global dayclr
+    match int(inputdate.strftime("%w")):
+        case 1:
+            dayclr = 0xEE5555 #monday=danger
+        case 2:
+            dayclr = 0x5555EE #synesthesia
+        case 3:
+            dayclr = 0x888888 #dim white
+        case 4:
+            dayclr = 0xAA00EE #no synesthesia
+        case 5:
+            dayclr = 0x66ffcf #meenty
+        case _:
+            dayclr = 0xFFAA00 #orange
+def blink(timeless):
+    if (int(timeless.strftime("%S")) % 3 == 0):
+        return 0
+    else:
+        return 1
+#multiply with color to make text blink
 coloring()
 freshdate(datetime.datetime.now())
-line1 = adafruit_display_text.label.Label(terminalio.FONT,color=hexclr, text=con1)
-line1.x = d.width
-line1.y = 8
-line2 = adafruit_display_text.label.Label(terminalio.FONT,color=0xffffff, text=con2)
-line2.x = d.width
-line1.y = 24
+line1 = {}
+line2 = {}
+def linearthings():
+    global line1, line2
+    line1 = adafruit_display_text.label.Label(terminalio.FONT, text=f"time")
+    line1.x = 2
+    line1.y = 8
+    line1.color = decclr
+    line2 = adafruit_display_text.label.Label(terminalio.FONT,color=0xff00ff, text=f"date")
+    line2.x = 2
+    line2.y = 24
+linearthings()
 g = displayio.Group()
-g.append(line1)
-g.append(line2)
-d.root_group = g
+def groupie():
+    global g
+    g.append(line1)
+    g.append(line2)
+groupie()
+d.root_group=g
+alarm_hour = 7 #24hr time
+alarm_minute_begin = 15
+alarm_minute_end = 20 # example, blinks 7:15-7:20
+def t(timeless, str):
+    return int(timeless.strftime(str))
+def testalarm(ti):
+    if (alarm_hour == t(ti, "%H")):
+        print("alarm hour")
+        if (t(ti,"%M") >= alarm_minute_begin) and (t(ti,"%M") <= alarm_minute_end):
+            print(f"alarm active {t(ti,"%M")} min")
+            return 1*blink(ti)
+        else:
+            return 1
+    else:
+        return 1
 while True:
-    freshdate(datetime.datetime.now())
+    #dt = datetime.datetime(2025,2,1)
+    # ^^ test day colors
+    dt = datetime.datetime.now()
+    freshdate(dt)
     coloring()
-    d.refresh(minimum_frames_per_second=0)
+    decideday(dt)
+    line1.text = con1
+    line2.text = con2
+    line1.color = decclr*testalarm(dt)
+    line2.color = dayclr
+    #linearthings()
+    d.refresh(minimum_frames_per_second=5)
+    #sleeping breaks this!!! what the devils?
+    #actually nvm, its linearthings
     time.sleep(0.2)
