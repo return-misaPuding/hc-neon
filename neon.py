7ff2ee103ef0b3a45da7f315272a579b4b830f42
return-misaPuding <70533713+return-misaPuding@users.noreply.github.com>
2025-02-01 05:33:36 +0100

    bztd speedrun

---
+++ neon.py
@@ -0,0 +1,23 @@
+#64x32 display with a rainbow animation, probably a clock function too
+import adafruit_display_text.label
+import board
+import displayio
+import framebufferio
+import rgbmatrix
+import terminalio
+import colorsys
+displayio.release_displays()
+m = rgbmatrix.RGBMatrix(width=64, height=32, bit_depth=4,rgb_pins=[board.D6, board.D5, board.D9, board.D11, board.D10, board.D12],
+    addr_pins=[board.A5, board.A4, board.A3, board.A2],
+    clock_pin=board.D13, latch_pin=board.D0, output_enable_pin=board.D1)
+d = framebufferio.FramebufferDisplay(m, auto_refresh=False)
+clr = 0.0
+hexclr= 0xffffff
+def coloring():
+    global hexclr
+    clr += 0.1 #rainbow
+    r,g,b = colorsys.hsv_to_rgb(clr,1,1)
+    hexclr = int(hex(r*256^3+g*256^2+b*256))
+coloring()
+line1 = adafruit_display_text.label.Label(terminalio.FONT,color=hexclr)
+while True:
